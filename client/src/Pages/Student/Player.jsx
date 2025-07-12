d7d3191ddfc11c558dd6b311ed1cc906b9c29f77
Manveer90 <mnveer89@gmail.com>
2025-07-12 13:37:03 +0530

    done

--- client/src/Pages/Student/Player.jsx
+++ client/src/Pages/Student/Player.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "./../../Context/AppContext";
 import { useParams } from "react-router-dom";
